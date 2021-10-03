3fbaccea59ed32e0c735ae62f145eac809868d14
PituPituGB <kopec.pawel@outlook.com>
2021-10-03 12:14:09 +0100

    Remove some code

--- VSCode Configuration Tutorial/test.py
+++ VSCode Configuration Tutorial/test.py
@@ -4,21 +4,9 @@ import sys
 import requests
 from os import rename
 
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_great):
-    greeting = "Hello {}".format(who_to_great)
-    return greeting
-
-
-print(greet("World"))
-print(greet("Corey"))
-
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
 # %%
 
 # %%
